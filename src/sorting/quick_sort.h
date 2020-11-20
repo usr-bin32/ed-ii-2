411cb2f26f4abe731ea3ad769dca4ec5da91b8f0
Gabriel <71942269+usr-bin32@users.noreply.github.com>
2020-11-20 16:04:10 -0300

    adicionar algoritmo de ordenação

---
+++ src/sorting/quick_sort.h
@@ -0,0 +1,46 @@
+#include <cstdlib>
+
+// particiona o vetor e o rearranja de forma que todos os elementos anteriores ao pivô sejam menores que ele e todos os posteriores sejam maiores.
+template <typename T>
+int slice(T *arr, int first, int end) {
+    int n = (end + first);
+    T pivot = arr[n / 2];
+
+    int i = first - 1, j = end + 1;
+
+    do {
+        do {
+            i++;
+            comparisons++;
+        } while (arr[i] < pivot);
+
+        do {
+            j--;
+            comparisons++;
+        } while (arr[j] > pivot);
+
+        if (i < j) {
+            T aux = arr[i];
+            arr[i] = arr[j];
+            arr[j] = aux;
+            swaps++;
+        }
+    } while (i < j);
+    return j;
+}
+
+template <typename T>
+void quick_sort(T *arr, int first, int end) {
+    if (first < end) {
+        int s = slice(arr, first, end);
+        // chamada recursiva para ordenar o subvetor de elementos menores
+        quick_sort(arr, first, s);
+        // chamada recursiva para ordenar o subvetor de elementos maiores
+        quick_sort(arr, s + 1, end);
+    }
+}
+
+template<typename T>
+void quick_sort(T *arr, size_t size) {
+    quick_sort(arr, 0, size - 1);
+}
