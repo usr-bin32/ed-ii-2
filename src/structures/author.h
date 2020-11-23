06c2e7fac6c9db7f31344561d906c90d6fd67335
Gabriel <71942269+usr-bin32@users.noreply.github.com>
2020-11-23 19:01:04 -0300

    corrigir estrutura de autor

--- src/structures/author.h
+++ src/structures/author.h
@@ -1,4 +1,13 @@
 struct author {
     int id;
     int occurrences = 0;
+
+    bool operator>(const author &a2);
+    bool operator>=(const author &a2);
+
+    bool operator<(const author &a2);
+    bool operator<=(const author &a2);
+
+    bool operator==(const author &a2);
+    bool operator!=(const author &a2);
 };
