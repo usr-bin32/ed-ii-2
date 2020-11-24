210c2acb6dd41c75c66d5f2ecf1e856074dd1589
Gabriel <71942269+usr-bin32@users.noreply.github.com>
2020-11-24 16:51:17 -0300

    adicionar orientações

--- src/tests/hashing.h
+++ src/tests/hashing.h
@@ -11,6 +11,8 @@
 #include "../structures/author.h"
 #include "../structures/book.h"
 
+// Aqui, ao invés de ler para um vector, ler direto para uma hash_table de
+// autores
 void read_books(std::vector<book> &books) {
     csv_parser parser("./res/books.csv");
 
@@ -23,6 +25,10 @@ void read_books(std::vector<book> &books) {
         book b;
 
         parser.get(0, b.authors);
+
+        // já incrementar na tabela de autores logo aqui
+        // cuidado com os autores inexistentes. Você deve lidar com eles aqui
+
         parser.get(1, b.bestsellers_rank);
         parser.get(2, b.categories);
         parser.get(3, b.edition);
@@ -52,7 +58,7 @@ void read_authors(std::vector<std::pair<int, std::string>> &authors) {
         parser.get(0, id);
         parser.get(1, name);
 
-        authors.push_back( std::make_pair(id, name));
+        authors.push_back(std::make_pair(id, name));
     }
 }
 
@@ -67,41 +73,47 @@ void test_hashing() {
     std::cin >> n;
 
     int m;
-    do{
+    do {
         std::cout << "Insira quantos autores deseja imprimir: ";
         std::cin >> m;
-    }while(m>n);
+    } while (m > n);
 
-    int tam_books = (n + n*0.20), tam_authors = (n + n*0.35), tam_all_authors; 
+    int tam_books = (n + n * 0.20), tam_authors = (n + n * 0.35),
+        tam_all_authors;
+
+    // ler todos os autores do CSV para uma hash_table<author>
+    // a struct, que está na master, tem o seguinte formato:
+    // { int id; std::string name; int occurrences = 0; }
 
     std::vector<std::pair<int, std::string>> all_authors;
     read_authors(all_authors);
-    tam_all_authors = all_authors.size() + all_authors.size()*0.20;
+    tam_all_authors = all_authors.size() + all_authors.size() * 0.20;
 
     hash_table<std::string> author_names(tam_all_authors);
 
     hash_table<book> hash_books(tam_books);
     hash_table<author> hash_authors(tam_authors);
     std::vector<book> books;
-    read_books(books); 
+    read_books(books);
     shuffle(books);
     books.resize(n);
 
     // armazenando os livros lidos em uma hash table
-    // criando uma nova hash_table com key = id e value = author { id,  contagem }
+    // criando uma nova hash_table com key = id e value = author { id,  contagem
+    // }
     for (auto &b : books) {
 
         hash_books.insert(b.id, b);
-       
+
         for (int id : b.authors) {
 
             author a;
             a.id = id;
             a.occurrences = 1;
 
-            if(hash_authors.search(id)){
-                hash_authors.search(id)->occurrences += 1; 
-            }else{
+            if (hash_authors.search(id)) {
+                hash_authors.search(id)->occurrences += 1;
+            } else {
                 hash_authors.insert(id, a);
             }
         }
@@ -116,6 +128,9 @@ void test_hashing() {
     quick_sort(author_vec.data(), author_vec.size());
 
     for (int i = 0; i < m && i < author_vec.size(); i++) {
+        // aqui, bastará pegar o autor:
+        // author_vec[i]
+        // std::cout << author_vec[i].name << ... << author_vec[i].occurrences
         if (author_names.search(author_vec[i].id) == nullptr) {
             m++;
             continue;
@@ -125,5 +140,3 @@ void test_hashing() {
                   << author_vec[i].occurrences << std::endl;
     }
 }
-
-
